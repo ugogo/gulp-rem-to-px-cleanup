05dee8ab33213983fc49c126a330fdda0b28e133
onaliugo <hello@onali-ugo.fr>
2015-05-25 17:27:30 +0200

    0.1.0 | Use rem-to-px-cleanup package, add example

---
+++ example/Gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var remToPx = require('../index.js');
+var rename = require('gulp-rename');
+
+gulp.task('default', function () {
+	gulp.src(['app.css', 'app_2.css'])
+		.pipe(remToPx({
+			baseFontSize: 2
+		}))
+		.pipe(rename({
+			suffix: '-ie'
+		}))
+		.pipe(gulp.dest('./'))
+});
